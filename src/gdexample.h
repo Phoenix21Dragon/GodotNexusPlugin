5c30ad43e265c9072804e7c4a46ccf8e3b5e2b30
Jason Dippmann <jasondippmann@gmail.com>
2025-07-09 14:26:07 +0200

    add Property from GDExtension Tutorial

--- src/gdexample.h
+++ src/gdexample.h
@@ -10,6 +10,7 @@ class GDExample : public Sprite2D {
 
 private:
 	double time_passed;
+	double amplitude;
 
 protected:
 	static void _bind_methods();
@@ -19,6 +20,8 @@ public:
 	~GDExample();
 
 	void _process(double delta) override;
+	void set_amplitude(const double p_amplitude);
+	double get_amplitude() const;
 };
 
 }
