3e4c660795ca606dadc1c52123c4a6d5216e7f4e
Jason Dippmann <jasondippmann@gmail.com>
2025-07-09 14:39:05 +0200

    add Signal from GDExtension Tutorial

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -8,6 +8,10 @@ void GDExample::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_amplitude", "p_amplitude"), &GDExample::set_amplitude);
 
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "amplitude"), "set_amplitude", "get_amplitude");
+
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "speed", PROPERTY_HINT_RANGE, "0,20,0.01"), "set_speed", "get_speed");
+
+	ADD_SIGNAL(MethodInfo("position_changed", PropertyInfo(Variant::OBJECT, "node"), PropertyInfo(Variant::VECTOR2, "new_pos")));
 }
 
 GDExample::GDExample() {
@@ -29,6 +33,13 @@ void GDExample::_process(double delta) {
 	);
 
 	set_position(new_position);
+
+	time_emit += delta;
+	if (time_emit > 1.0) {
+		emit_signal("position_changed", this, new_position);
+
+		time_emit = 0.0;
+	}
 }
 
 void GDExample::set_amplitude(const double p_amplitude) {
